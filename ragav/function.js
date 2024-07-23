5f358ce0b4a4aa8e3f90349223642cfa0ff3902b
someshwar reddy <somesh.satish@gmail.com>
2024-07-23 21:21:52 +0530

    tejender html

---
+++ ragav/function.js
@@ -0,0 +1,39 @@
+// functions
+
+// named function 
+// unnamed or function expression or anonymous
+// iife
+// call back
+// high order
+// arrow or lamda
+// aync 
+// genrator function
+// method function
+// pure functions
+// impure functions
+
+
+
+
+//iife
+
+(function(){
+
+})()
+
+function raghu() { // function declaration
+    console.log('  i am a named function');
+}
+
+// calling function or invoking
+
+var fexpression = function () {
+    console.log(' i am function expression')
+}; // function expression
+
+fexpression();
+
+console.log(typeof (fexpression))
+
+
+
