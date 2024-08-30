65a6682642f556a54913beaf9e390075a845405a
someshwar reddy <somesh.satish@gmail.com>
2024-08-30 17:15:18 +0530

    excution

---
+++ tejander/javascript/datatypes.js
@@ -0,0 +1,33 @@
+var a;
+console.log(typeof (a)); // which is undefined data type
+
+var a = 10;
+console.log(typeof(a)) // which is  number type 
+
+var a ="tejinder";
+console.log(typeof(a)); // which is string
+
+var a = "";
+console.log(typeof(a)); // which is string datatype
+
+var a = false;
+console.log(typeof(a));  // which is boolean type
+
+var a = null;
+console.log(typeof(a)); // which is null type
+
+var a = Symbol
+
+
+// non primitive
+var a = [];
+console.log(typeof(a));
+
+var a = {};
+console.log(typeof(a));
+
+var a = function(){};
+console.log(typeof(a))
+
+
+let b =10;
---
+++ tejander/javascript/varibles.js
@@ -0,0 +1,19 @@
+// excution context
+
+// var let const
+
+var a ="tejinder";
+let b ="value unavilable in global creation phase"
+const c = " i am const value once defined unchangeble "
+
+//  let is a block scope 
+// var is function scope 
+var gloabal = "i am global"
+
+function bb() {
+
+  var a = "function excution"
+  
+}
+
+bb();
